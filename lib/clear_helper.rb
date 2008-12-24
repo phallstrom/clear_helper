308943116f2e8b4d8704ede37f1882c8a258e00a
Philip Hallstrom <philip@pjkh.com>
2008-12-24 13:46:46 -0800

    update clear helper to take an hash as an argument whose keys are stringified and turned into the style specific attributes

--- lib/clear_helper.rb
+++ lib/clear_helper.rb
@@ -7,15 +7,28 @@ module ActionView
       #
       # 
       #
-      def clear(what = 'both', height = '0')
-        unless %w[left right both].include?(what.to_s)
-          height = what
-          what = 'both'
+      def clear(clear = 'both', height = '0')
+        options = {}
+        if clear.is_a?(Hash)
+          options = clear.stringify_keys.reverse_merge!('clear' => 'both', 'height' => '0')
+        else
+          unless %w[left right both].include?(clear.to_s)
+            height = clear
+            clear = 'both'
+          end
+          options['height'] = height
+          options['clear'] = clear
         end
-        if height.to_i.to_s == height.to_s
-          height = "#{height}px" unless height.to_s == '0'
+        if options['height'].to_i.to_s == options['height'].to_s
+          options['height'] = "#{options['height']}px" unless options['height'].to_s == '0'
         end
-        %Q{<div style="clear:#{what}; height: #{height}; max-height: #{height}; font-size: #{height}; line-height: #{height};">&nbsp;</div>}
+
+        options.reverse_merge!('max-height' => options['height'],
+                               'font-size' => options['height'],
+                               'line-height' => options['height'])
+
+        # the sort is really just so the tests pass easily
+        %Q{<div style="#{options.map {|e| "#{e.first}: #{e.last}" }.sort.join('; ')}">&nbsp;</div>}
       end
 
     end
